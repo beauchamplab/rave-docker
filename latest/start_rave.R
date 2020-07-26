014fb949e592e53dec2bd035b025f4c62c44f301
dipterix <zw23@rice.edu>
2020-07-26 00:37:13 -0500

    update

--- latest/start_rave.R
+++ latest/start_rave.R
@@ -6,13 +6,19 @@
 # Start RAVE main application
 
 library(docopt)
+library(rave)
+
+local({
+  user <- system('echo $(whoami)', intern = TRUE, wait = TRUE)
+  cat('Running as', user, '\n')
+})
 
 ## configuration for docopt
-doc <- "Usage: start_rave [-h] [--host HOST] [-p PORT] [-n NCPUS] [-x] [--] [PACKAGES ...]
+doc <- "Usage: start_rave [-h] [-a HOST] [-p PORT] [-n NCPUS] [-x] [--] [PACKAGES ...]
 
---host HOST         location in which to install [default: 127.0.0.1]
+-a --host HOST      location in which to install [default: 0.0.0.0]
 -p --port PORT      install suggested dependencies as well [default: 6767]
--n --ncpus NCPUS    number of processes to use for parallel install [default: 1]
+-n --ncpus NCPUS    number of processes to use for parallel install [default: NA]
 -t --token TOKEN    token of the application [default: NULL]
 
 -h --help           show this help text
@@ -30,7 +36,20 @@ if(is.na(opt$port) || opt$port < 1000 || opt$port > 65535){
   q("no")
 }
 
-opt$ncpus <- as.integer(opt$ncpus)
+opt$ncpus <- local({
+  n <- as.integer(opt$ncpus)
+  if(length(n)){
+    n <- as.integer(n)
+    if(raveio::is_valid_ish(n, max_len = 1)){
+      cat('[ncores]: Using user-defined settings -', n)
+    } else {
+      n <- Sys.getenv('NCPUS')
+      cat('[ncores]: Using system settings -', n)
+    }
+  }
+  cat('\n')
+  return(as.integer(n))
+})
 opt$aval_ncpu <- future::availableCores()
 if(is.na(opt$ncpus) || opt$ncpus < 1 ){
   cat("[ncores]: Error: number of CPU must be integer >= 1\n")
@@ -45,18 +64,48 @@ if(!length(opt$token) || is.na(opt$token) || opt$token %in% c('NA', '', 'NULL'))
   opt$token <- NULL
 }
 
-# set options
-rave::rave_options(
-  'max_worker' = opt$ncpus,
-  'disable_startup_speed_check' = TRUE,
-  'data_dir' = '/data/rave_data/data_dir',
-  'raw_data_dir' = '/data/rave_data/raw_dir',
-  'subject_cache_dir' = '/data/rave_data/cache_dir',
-  'bids_data_dir' = '/data/rave_data/bids_dir'
-)
+arrange_modules()
+arrange_data_dir()
+
+local({
+  # get system environment to set dirs
+  data_dir <- Sys.getenv('RAVEDATA', unset = '/data/rave_data/data_dir')
+  raw_data_dir <- Sys.getenv('RAVERAW', unset = '/data/rave_data/raw_dir')
+  tensor_temp_path <- Sys.getenv('RAVECACHE', unset = '/data/rave_data/cache_dir')
+  bids_data_dir <- Sys.getenv('RAVEBIDS', unset = '/data/rave_data/bids_dir')
+
+  data_dir <- raveio::dir_create2(data_dir)
+  raw_data_dir <- raveio::dir_create2(raw_data_dir)
+  tensor_temp_path <- raveio::dir_create2(tensor_temp_path)
+  bids_data_dir <- raveio::dir_create2(bids_data_dir)
+
+  # set options
+  rave_options(
+    'max_worker' = opt$ncpus,
+    'disable_startup_speed_check' = TRUE,
+    'data_dir' = data_dir,
+    'raw_data_dir' = raw_data_dir,
+    'tensor_temp_path' = tensor_temp_path,
+    'bids_data_dir' = bids_data_dir
+  )
+  print(raveio::raveio_getopt())
+})
+
+# finalize installation
+tryCatch({
+  rave::finalize_installation(upgrade = 'never')
+}, error = function(e){
+  cat('Error while finalize installation. Reason:\n', e$message, '\n')
+})
 
 # Run rave
-app <- rave::start_rave(launch.browser = FALSE, host = opt$host, port = opt$port, token = opt$token)
+app <-
+  start_rave(
+    launch.browser = FALSE,
+    host = '0.0.0.0',
+    port = opt$port,
+    token = opt$token
+  )
 print(app)
 
 q("no")
